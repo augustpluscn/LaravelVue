8b4ef9c58250336192b55dd61c62d86358701d75
august <1@augustor.com>
2021-11-08 22:19:35 +0800

    加入全局样式

--- resources/js/src/main.js
+++ resources/js/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入全局样式
+import '@/assets/css/index.scss'
 
 // 全局引入按需引入UI库 vant
 import '@/plugins/vant'
